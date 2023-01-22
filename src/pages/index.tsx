5ab1c94d5382bac3f14d6cf0d20365d2b102e685
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-01-21 22:07:19 -0500

    landing update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,35 @@
 import styles from "../styles/index.module.scss";
-import Image from "next/image";
-import { NextPage } from "next/types";
-import useSWR from "swr";
-import { env } from "../env/client.mjs";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
-import SvgAttack from "../components/icons/Attack";
-import SvgDefense from "../components/icons/Defense";
 import Link from "next/link";
-import dayjs from "dayjs";
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-const URL = env.NEXT_PUBLIC_URL;
-const API = URL + "/api/sheets";
 
 const Landing = (props: any) => {
-  const { data } = useSWR(API, fetcher, {
-    refreshWhenHidden: true,
-    refreshInterval: 10000,
-  });
-  if (!data) {
-    return <>Loading...</>;
-  }
   return (
     <div className={styles.linkBox}>
+      <h1>Welcome to Scoreboardish!</h1>
+
       <Link className={styles.link} href="/ingame">
-        In Game Scene
+        In Game
       </Link>
       <Link className={styles.link} href="/mapinfo">
-        Map Info Scene
+        Map Info
       </Link>
       <Link className={styles.link} href="/casters">
-        Casters Scene
+        Casters
       </Link>
       <Link className={styles.link} href="/startingsoon">
-        Starting Soon Scene
+        Starting Soon
       </Link>
       <Link className={styles.link} href="/credits">
-        Credits Scene
+        Credits
       </Link>
+      <p>
+        - These pages only display properly on a 1920x1080 canvas.
+        <br />
+        - For use in broadcast please add one of these to an OBS browser source
+        with the aforementioned dimensions.
+        <br />- If you need these in a higher resolution, the current solution
+        is to create the browser source at 1920x1080 and then stretch it to the
+        desired size.
+      </p>
     </div>
   );
 };
