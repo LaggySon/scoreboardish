00290ca5189f7706bdb84738aa97ad8ca472e039
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-05-03 13:53:41 -0400

    timers for ht scenes

--- src/pages/TranquilityGaming/htanalysis.tsx
+++ src/pages/TranquilityGaming/htanalysis.tsx
@@ -6,6 +6,12 @@ import TranqScoreboard from "../../components/tranqScoreboard";
 import TranqCaster from "../../components/tranqCouchCaster";
 import TranqTelestrator from "../../components/tranqTelestrator";
 import TranqScoreboardEW from "../../components/tranqScoreboardEW";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import { useEffect, useState } from "react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const URL = env.NEXT_PUBLIC_URL;
@@ -19,6 +25,30 @@ const Casters = (props: any) => {
 
   const users = data?.cams.slice(0, 4);
   console.log(users);
+
+  //TIMER STUFF
+  dayjs.extend(duration);
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.extend(advancedFormat);
+
+  const endTime = dayjs(data?.match?.dateTime * 1000 + 120000);
+
+  const [dayjsLeft, setDayjsLeft] = useState(
+    dayjs.duration(endTime.diff(dayjs()))
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const diffMilli = endTime.diff(dayjs());
+      if (diffMilli > 0) {
+        setDayjsLeft(dayjs.duration(diffMilli));
+      } else {
+        setDayjsLeft(dayjs.duration(0));
+      }
+    }, 250);
+    return () => clearInterval(interval);
+  });
 
   if (!data) {
     return <>Loading...</>;
@@ -37,6 +67,7 @@ const Casters = (props: any) => {
           }
         `}
       </style>
+      <div className={styles.startingTimer}>{dayjsLeft.format("mm:ss")}</div>
       {["harmony", "discord", "transcendence", "admin pugs"].includes(
         data?.match?.tier.toLowerCase()
       ) ? (
