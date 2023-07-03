81e1e2cd845b03dcc306d5830686b4f8e009c702
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-07-03 16:44:45 -0400

    fake tally

--- src/lib/obs.ts
+++ src/lib/obs.ts
@@ -1,24 +1,34 @@
 import OBSWebSocket from "obs-websocket-js";
 
-const obs = new OBSWebSocket();
+class obsSocket {
+  obs = new OBSWebSocket();
 
-async function connect(): Promise<any> {
-  try {
-    const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
-      "ws://127.0.0.1:4455",
-      "password",
-      {
-        rpcVersion: 1,
-      }
-    );
-    console.log(
-      `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`
-    );
-    console.log(await obs.call("GetCurrentProgramScene"));
-    return obs;
-  } catch (error: any) {
-    console.error("Failed to connect", error.code, error.message);
-  }
+  scene = "";
+
+  connect = async () => {
+    try {
+      const { obsWebSocketVersion, negotiatedRpcVersion } =
+        await this.obs.connect("ws://192.168.1.152:4444", "password", {
+          rpcVersion: 1,
+        });
+      console.log(
+        `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`
+      );
+      console.log(await this.obs.call("GetCurrentProgramScene"));
+      return this.obs;
+    } catch (error: any) {
+      console.error("Failed to connect", error.code, error.message);
+    }
+  };
+
+  getScene = async () => {
+    try {
+      const currscene = await this.obs.call("GetSceneList");
+      this.scene = currscene.currentProgramSceneName;
+    } catch (error: any) {
+      console.error("Failed to connect", error.code, error.message);
+    }
+  };
 }
 
-export { obs, connect };
+export { obsSocket };
--- src/pages/TranquilityGaming/tally.tsx
+++ src/pages/TranquilityGaming/tally.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next/types";
-import useSWR from "swr";
 import { env } from "../../env/client.mjs";
-import styles from "../../styles/TranquilityGaming/obs.module.scss";
-import Image from "next/image.js";
-import { obs, connect, getScenes } from "../../lib/obs";
+import { obsSocket } from "../../lib/obs";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import io from "socket.io-client";
+
+let socket: any;
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const URL = env.NEXT_PUBLIC_URL;
@@ -12,21 +13,88 @@ const API =
   URL + `/api/sheets?sheet=15lldKBTIAAzgKlg7SizMCJkx68OVyOiMlRonJJsHq5o`;
 
 const InGame: NextPage<PageProps> = (props) => {
-  const [scene, setScene] = useState("Init");
-  useEffect(() => {}, []);
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+
+  useEffect(() => {
+    socketIntializer();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  async function socketIntializer() {
+    await fetch("/api/socket");
+
+    socket = io();
+
+    socket.on("receive-message", (data: any) => {
+      setAllMessages((pre): any => [...pre, data]);
+    });
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    console.log("emitted");
+
+    socket.emit("send-message", {
+      username,
+      message,
+    });
+
+    setMessage("");
+  }
+
+  //OBS Websocket stuff
+  const obs = new obsSocket();
+  useEffect(() => {
+    console.log(obs.connect());
+    obs.getScene();
+    console.log(obs.scene);
+  }, []);
+
+  //Get URL parameters
+  const router = useRouter();
+  const [query, setQuery] = useState({});
+  useEffect(() => {
+    if (!router.isReady) return;
+    const query = router.query;
+    setQuery(query);
+  }, [router.isReady, router.query]);
 
   return (
-    <>
-      <style jsx global>
-        {`
-          :root {
-            font-family: "Industry";
-            font-weight: normal;
-          }
-        `}
-      </style>
-      <p>{currScene}</p>
-    </>
+    <div>
+      <h1>Tranquility Messaging Application</h1>
+      <h1>Enter a username</h1>
+
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+
+      <br />
+      <br />
+
+      <div>
+        {allMessages.map(({ username, message }, index) => (
+          <div key={index}>
+            {username}: {message}
+          </div>
+        ))}
+
+        <br />
+
+        <form onSubmit={handleSubmit}>
+          <input
+            name="message"
+            placeholder="enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            autoComplete={"off"}
+          />
+        </form>
+      </div>
+    </div>
   );
 };
 
