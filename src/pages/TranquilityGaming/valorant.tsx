655ccb96d6a5bb1f3fc83d2d420c9cb48868e94c
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-07-31 18:44:43 -0400

    val

---
+++ src/pages/TranquilityGaming/valorant.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from "next/types";
+import useSWR from "swr";
+import { env } from "../../env/client.mjs";
+import TranqScoreboard from "../../components/tranqScoreboard";
+import TranqScoreboardEW from "../../components/tranqScoreboardEW";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TranqValScoreboard from "../../components/tranqValorant";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const URL = env.NEXT_PUBLIC_URL;
+const API = URL + `/api/sheets`;
+
+const InGame: NextPage<PageProps> = (props) => {
+  //Get URL parameters
+  const router = useRouter();
+  const [query, setQuery] = useState({ sheet: "" });
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { sheet } = router.query;
+    setQuery({ sheet: String(sheet) });
+  }, [router.isReady, router.query]);
+
+  const { data } = useSWR(API + `?sheet=${query?.sheet}`, fetcher, {
+    refreshWhenHidden: true,
+    refreshInterval: 10000,
+  });
+  if (!data) {
+    return <>Loading...</>;
+  }
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --team1PrimaryColor: ${data?.teams?.team1.primaryCol ??
+            "var(--tranqBlue)"};
+            --team1SecondaryColor: ${data?.teams?.team1.secondaryCol ??
+            "var(--tranqBlue)"};
+            --team2PrimaryColor: ${data?.teams?.team2.primaryCol ??
+            "var(--tranqYellow)"};
+            --team2SecondaryColor: ${data?.teams?.team2.secondaryCol ??
+            "var(--tranqYellow)"};
+            font-family: "Industry";
+            font-weight: normal;
+          }
+        `}
+      </style>
+      <TranqValScoreboard data={data}></TranqValScoreboard>
+    </>
+  );
+};
+
+export default InGame;
